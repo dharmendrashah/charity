53fe45a9fbe6de3bd3a490dee979bb372ddbbd6a
Dharmendra Shah <dharmendrashahz10s@outlook.com>
2020-03-28 20:51:36 +0530

    this is charity website

---
+++ resources/lang/en/home.php
@@ -0,0 +1,16 @@
+<?php
+//start
+return[
+    'home' => 'home',
+    'title'  =>  "GIVE A HAND TO MAKE THE BETTER WORLD",
+    'joinNow' => 'Join now',
+    'donateNow' => 'Donate Now',
+    'donationNow' => 'Donation now',
+    'helpSupport' => 'HELP & SUPPORT',
+    'adoption' => 'ADOPTION',
+    'volunteering' => 'VOLUNTEERING',
+    'education' => 'EDUCATION',
+    'grandprojects' => 'FOUNDATION GRANTS PROJECTS',
+    'grandprojectTitle' => ''
+];
+//end
---
+++ resources/views/parcels/donation.blade.php
@@ -0,0 +1,34 @@
+<section class="ftco-section bg-light">
+    <div class="container">
+        <div class="row justify-content-center pb-3">
+  <div class="col-md-7 heading-section text-center ftco-animate">
+      <span class="subheading">Foundation Grants Projects</span>
+    <h2 class="mb-4">Gifts into Service Project that Change</h2>
+    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+  </div>
+</div>
+<div class="row justify-content-center ftco-animate">
+    <div class="col-md-8">
+        <div class="featured-causes">
+            <div class="progress" style="height:50px">
+                      <div class="progress-bar progress-bar-striped" style="width:65%; height:50px"></div>
+                    </div>
+                    <div class="text mt-4 d-md-flex">
+                        <div class="one d-flex">
+                            <div class="mr-4">
+                                <h2>65%</h2>
+                            </div>
+                            <div class="goal">
+                                <p class="d-flex"><span>Collected:</span><span>$380,000</span></p>
+                                <p class="d-flex"><span>Goal:</span><span>$600,000</span></p>
+                            </div>
+                        </div>
+                        <div class="one text-md-right">
+                            <p><a href="#" class="btn btn-primary">Donate now</a></p>
+                        </div>
+                    </div>
+        </div>
+    </div>
+</div>
+    </div>
+</section>
